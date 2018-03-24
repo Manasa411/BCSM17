4d4ea0fd81145868ff38efb78d9867d0c3c4011c
Manasa411 <37699272+Manasa411@users.noreply.github.com>
2018-03-23 21:20:42 -0700

    uwgiguqwgiudgd

--- BCSM17/src/page/Loginpage.java
+++ BCSM17/src/page/Loginpage.java
@@ -8,8 +8,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 
-import junit.framework.Assert;
 
+@SuppressWarnings("unused")
 public class Loginpage 
 {
  
